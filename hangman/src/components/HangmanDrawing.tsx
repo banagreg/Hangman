69130e4f23af0cc4a3d90800f313b96639a59e92
banagreg <bogdanovagergana97@gmail.com>
2024-01-01 15:45:24 +0200

    Implemented Hangman drawing

--- hangman/src/components/HangmanDrawing.tsx
+++ hangman/src/components/HangmanDrawing.tsx
@@ -1,95 +1,16 @@
-const HEAD = (
-	<div style={{
-		width: '50px',
-		height: '50px',
-		borderRadius: '100%',
-		border: '10px solid black',
-		position: 'absolute',
-		top: '50px',
-		right: '-30px'
-	}} 
-	/>
-)
+import { HEAD, BODY, RIGHT_ARM, LEFT_ARM, RIGHT_LEG, LEFT_LEG } from '../constants/bodyParts.js'
 
-const BODY = (
-	<div style={{
-		width: '10px',
-		height: '100px',
-		background: 'black',
-		position: 'absolute',
-		top: '120px',
-		right: '0px'
-	}} 
-	/>
-)
+const BODY_PARTS = [HEAD, BODY, RIGHT_ARM, LEFT_ARM, RIGHT_LEG, LEFT_LEG]
 
-const RIGHT_ARM = (
-	<div style={{
-		width: '100px',
-		height: '10px',
-		background: 'black',
-		position: 'absolute',
-		top: '150px',
-		right: '-100px',
-		rotate: '-30deg',
-		transformOrigin: 'left bottom'
-	}} 
-	/>
-)
+type HangmanDrawingProps = {
+	numberOfGuesses: number,
+}
 
-const LEFT_ARM = (
-	<div style={{
-		width: '100px',
-		height: '10px',
-		background: 'black',
-		position: 'absolute',
-		top: '150px',
-		right: '10px',
-		rotate: '30deg',
-		transformOrigin: 'right bottom'
-	}} 
-	/>
-)
-
-const RIGHT_LEG = (
-	<div style={{
-		width: '100px',
-		height: '10px',
-		background: 'black',
-		position: 'absolute',
-		top: '210px',
-		right: '-90px',
-		rotate: '60deg',
-		transformOrigin: 'left bottom'
-	}} 
-	/>
-)
-
-const LEFT_LEG = (
-	<div style={{
-		width: '100px',
-		height: '10px',
-		background: 'black',
-		position: 'absolute',
-		top: '210px',
-		right: 0,
-		rotate: '-60deg',
-		transformOrigin: 'right bottom'
-	}} 
-	/>
-)
-
-
-
-export function HangmanDrawing() {
+export function HangmanDrawing({ numberOfGuesses }: HangmanDrawingProps) {
 	return (
 		<div style={{ position: 'relative' }}>
-			{HEAD}
-			{BODY} 
-			{RIGHT_ARM}
-			{LEFT_ARM}
-			{RIGHT_LEG}
-			{LEFT_LEG}
+			{BODY_PARTS.slice(0, numberOfGuesses)}
+			
 			<div style={{
 				height: '50px',
 				width: '10px',
